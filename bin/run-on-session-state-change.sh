d4a23be8da0d76fc3f7134047a81e55f7481bbea
Diego Lago <diego.lago.gonzalez@gmail.com>
2024-10-31 17:24:46 +0100

    Use spaces for indentation

--- bin/run-on-session-state-change.sh
+++ bin/run-on-session-state-change.sh
@@ -5,18 +5,18 @@ VERSION="0.1.0-20241031"
 VERBOSE=
 
 function error() {
-	echo "ERROR: $1" >&2
-	exit
+    echo "ERROR: $1" >&2
+    exit
 }
 
 function verbose() {
-	if [ -n "$VERBOSE" ]; then
-		echo "$1"
-	fi
+    if [ -n "$VERBOSE" ]; then
+        echo "$1"
+    fi
 }
 
 function show_help() {
-	cat <<EOT
+    cat <<EOT
 $(basename -- $0) v$VERSION -- Tool to run a script on lock/unlock screen."
 Usage: $(basename -- $0) [Options]
 Options:
@@ -28,57 +28,57 @@ EOT
 }
 
 function process_options() {
-	while getopts ":hl:u:v" opt; do
-		case "$opt" in
-			h)
-				show_help
-				exit 1
-			;;
-			l)
-				RUN_ON_LOCK="$OPTARG"
-			;;
-			u)
-				RUN_ON_UNLOCK="$OPTARG"
-			;;
-			v)
-				VERBOSE=yes
-			;;
-			\?)
-				error "Invalid option: -$OPTARG"
-			;;
-			:)
-				error "Option -$OPTARG requires an argument."
-			;;
-		esac
-	done
+    while getopts ":hl:u:v" opt; do
+        case "$opt" in
+            h)
+                show_help
+                exit 1
+            ;;
+            l)
+                RUN_ON_LOCK="$OPTARG"
+            ;;
+            u)
+                RUN_ON_UNLOCK="$OPTARG"
+            ;;
+            v)
+                VERBOSE=yes
+            ;;
+            \?)
+                error "Invalid option: -$OPTARG"
+            ;;
+            :)
+                error "Option -$OPTARG requires an argument."
+            ;;
+        esac
+    done
 }
 
 function check_options() {
-	if [ $1 == 0 ]; then
-		error "Invalid command line arguments. Use -h to see help."
-	fi
-	if [ -z "$RUN_ON_LOCK" -a -z "$RUN_ON_UNLOCK" ]; then
-		error "Either -l or -u options must be used."
-	fi
+    if [ $1 == 0 ]; then
+        error "Invalid command line arguments. Use -h to see help."
+    fi
+    if [ -z "$RUN_ON_LOCK" -a -z "$RUN_ON_UNLOCK" ]; then
+        error "Either -l or -u options must be used."
+    fi
 }
 
 function run_loop() {
-	while read line; do
-		case "$line" in
-			*"{'LockedHint': <true>}"*)
-				eval "$RUN_ON_LOCK"
-			;;
-			*"{'LockedHint': <false>}"*)
-				eval "$RUN_ON_UNLOCK"
-			;;
-		esac
-	done < <(gdbus monitor --system --dest org.freedesktop.login1)
+    while read line; do
+        case "$line" in
+            *"{'LockedHint': <true>}"*)
+                eval "$RUN_ON_LOCK"
+            ;;
+            *"{'LockedHint': <false>}"*)
+                eval "$RUN_ON_UNLOCK"
+            ;;
+        esac
+    done < <(gdbus monitor --system --dest org.freedesktop.login1)
 }
 
 function main() {
-	process_options "$@"
-	check_options $#
-	run_loop
+    process_options "$@"
+    check_options $#
+    run_loop
 }
 
 main "$@"
